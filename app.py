3d049f41aa5a81d9399303b81cba91cbd7abfb29
Srijon <mandal.srijon@gmail.com>
2021-08-04 20:35:52 -0700

    request.form might return none type (#41)
    * request.form might return none type
    
    * minor changes to forum page
    
    * forgot some things in previous commit
    
    * adding get request for scan api
    
    Co-authored-by: srijon-mandal <59291729+srijon-mandal@users.noreply.github.com>

--- app.py
+++ app.py
@@ -257,13 +257,13 @@ def api_signup():
     }
     return jsonify(return_data)
 
-@app.route('/api/scans', methods=["POST"])
+@app.route('/api/scans', methods=["GET","POST"])
 @token_required
 def api_find(userId):
     scans = db['scans']
-    lat = float(request.form.get('lat', 0))
-    long = float(request.form.get('long', 0))
-    radius = float(request.form.get('range', None))
+    lat = float(request.get_json().get('lat', 0))
+    long = float(request.get_json().get('long', 0))
+    radius = float(request.get_json().get('range', None))
     scans.create_index([('loc', '2dsphere')])
     result = []
     search = [
@@ -307,9 +307,9 @@ def api_find(userId):
 @app.route('/api/scans/all', methods=["POST"])
 def api_find_all():
     scans = db['scans']
-    lat = float(request.form.get('lat', 0))
-    long = float(request.form.get('long', 0))
-    radius = float(request.form.get('range', None))
+    lat = float(request.get_json().get('lat', 0))
+    long = float(request.get_json().get('long', 0))
+    radius = float(request.get_json().get('range', None))
     scans.create_index([('loc', '2dsphere')])
     result = []
     search = [
@@ -380,8 +380,8 @@ def api_vote(userId):
 def api_add(userId):
     scans = db['scans']
     f = request.files['image']
-    lat = float(request.form.get('lat'))
-    long = float(request.form.get('long'))
+    lat = float(request.get_json().get('lat'))
+    long = float(request.get_json().get('long'))
     filename = str(uuid4())
     title = request.form.get('title')
     des = request.form.get('des', None)
