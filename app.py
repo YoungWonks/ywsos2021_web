8c869be5644c9d9841cabf5c722b965425f7a28c
snek152 <kakanisnehil@gmail.com>
2021-08-13 17:15:29 -0700

    fixing not statement

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ def api_find(userId):
     ]        
     if radius:
         search[0]['$geoNear']['maxDistance'] = radius
-    elif notposition[0]:
+    elif not position[0]:
         search = [search[1], {
             '$sort': {
                 'vote': -1,
@@ -383,7 +383,7 @@ def api_find_all():
     ]
     if radius:
         search[0]['$geoNear']['maxDistance'] = radius
-    elif notposition[0]:
+    elif not position[0]:
         search = [{
             '$sort': {
                 'vote': -1,
