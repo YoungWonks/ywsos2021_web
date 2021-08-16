03f0c830af8d266201a838a4f82dc04aaaa31b23
NeilP5608 <neilp235094@gmail.com>
2021-08-15 20:48:01 -0400

    Updating test

--- test_cases/api_test.py
+++ test_cases/api_test.py
@@ -51,10 +51,12 @@ if res["error"] == "0":
     for repair in res['repairs']:
         real_url = 'http://127.0.0.1:5000' + repair['url']
         binary = requests.get(real_url).content
+        res2 = json.loads(requests.post('http://127.0.0.1:5000/api/scans/voted', headers=headers, json={"scan_id": str(repair['id'])}).content.decode())
         with open('images/read.jfif', 'wb') as f:
             f.write(binary)
         image = cv2.imread('images/read.jfif')
         cv2.imshow("Scans", image)
         print("Location is {}, {}".format(repair['position']['lat'], repair['position']['long']))
         print("Title is {}".format(repair['title']))
+        print("Downvote" if res2["voted"] else "Upvote")
         cv2.waitKey()
